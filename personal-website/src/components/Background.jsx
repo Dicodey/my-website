42d897e4173be59536abd0bdd4e8361f737e735c
Di <diderik.ypma@live.co.uk>
2026-01-19 10:16:28 +0000

    Move red brain to Let's Chat section; reduce page height to 3200px

--- personal-website/src/components/Background.jsx
+++ personal-website/src/components/Background.jsx
@@ -7,7 +7,7 @@ const Background = () => {
         { image: `${import.meta.env.BASE_URL}brain-yellow.png`, top: '20px', left: '-100px', size: '520px', delay: '0s' },
         { image: `${import.meta.env.BASE_URL}brain-green.png`, top: '1000px', right: '10%', size: '400px', delay: '-15s' },
         { image: `${import.meta.env.BASE_URL}brain-orange.png`, top: '2200px', right: '-15%', size: '440px', delay: '-5s' },
-        { image: `${import.meta.env.BASE_URL}brain-red.png`, top: '3400px', left: '5%', size: '360px', delay: '-10s' },
+        { image: `${import.meta.env.BASE_URL}brain-red.png`, top: '2800px', left: '-80px', size: '360px', delay: '-10s' },
     ];
 
     return (
