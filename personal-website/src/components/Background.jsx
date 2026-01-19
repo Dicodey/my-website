6017eece30490c0fc4212e8c1387fac8e11684b9
Di <diderik.ypma@live.co.uk>
2026-01-19 10:07:14 +0000

    adjust yellow brain position

--- personal-website/src/components/Background.jsx
+++ personal-website/src/components/Background.jsx
@@ -4,7 +4,7 @@ import './Background.style.css';
 
 const Background = () => {
     const blobs = [
-        { image: `${import.meta.env.BASE_URL}brain-yellow.png`, top: '-200px', left: '-250px', size: '520px', delay: '0s' },
+        { image: `${import.meta.env.BASE_URL}brain-yellow.png`, top: '-60px', left: '-150px', size: '520px', delay: '0s' },
         { image: `${import.meta.env.BASE_URL}brain-green.png`, top: '1000px', right: '10%', size: '400px', delay: '-15s' },
         { image: `${import.meta.env.BASE_URL}brain-orange.png`, top: '2200px', right: '-15%', size: '440px', delay: '-5s' },
         { image: `${import.meta.env.BASE_URL}brain-red.png`, top: '3400px', left: '5%', size: '360px', delay: '-10s' },
